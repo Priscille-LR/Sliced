9ddef2a9002a5e5f2400f14e78165c6256ad0660
Priscille Louis-rose <priscille.louisrose@gmail.com>
2022-08-22 14:36:06 +0200

    auth stucture

--- models/User.model.js
+++ models/User.model.js
@@ -1,16 +1,25 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
   {
+    email: {
+      type: String,
+      required: [true, 'Email is required.'],
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required.'],
+      trim: true,
+    },
     username: {
       type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
+      required: [true, 'Full Name is required.'],
+      trim: true,
     },
-    password: String,
   },
   {
-    // this second object adds extra properties: `createdAt` and `updatedAt`
     timestamps: true,
   }
 );
